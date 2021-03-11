fb4473657714f2326b9c2615e6c12d29811e793f
ritesh <riteshzeronity@gmail.com>
2021-03-11 12:13:13 +0530

    added fun

---
+++ web/main.js
@@ -0,0 +1,27 @@
+var idblock = document.getElementById("block1")
+var marginLeft = 0, marginTop = 0;
+var marginLeftLimit = 100, marginTopLimit = 47;
+var marginLeftState = true, marginTopState = true;
+
+function moveBlock(){
+    var marginLeftStringValue = marginLeft.toString();
+    var marginTopStringValue = marginTop.toString();
+    idblock.style.marginLeft = marginLeftStringValue + "%";
+    idblock.style.marginTop = marginTopStringValue + "%";
+    if (marginLeft == 0) marginLeftState = true;
+    if (marginTop == 0) marginTopState = true;
+    if (marginTop == marginTopLimit) marginTopState = false;
+    if (marginLeft == marginLeftLimit) marginLeftState = false;
+    if (marginLeftState == true){
+        marginLeft++;
+    } else{
+        marginLeft--;
+    }
+    if (marginTopState == true){
+        marginTop++;
+    } else{
+        marginTop--;
+    }
+}
+
+setInterval(moveBlock, 1);
